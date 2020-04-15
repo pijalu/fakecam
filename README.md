f7f1a1755643dbcf535e40a3519932eb7142e79e
Pierre Poissinger <pierre.poissinger@gmail.com>
2020-04-15 15:20:55 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FakeCam
+Stupid docker based on https://elder.dev/posts/open-source-virtual-background/ and HN article 
+
+## Install
+* Install  https://github.com/umlaeute/v4l2loopback
+* Run that docker image
+* use /dev/video20
---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo modprobe -r v4l2loopback
+sudo modprobe v4l2loopback devices=1 video_nr=20 card_label="v4l2loopback" exclusive_caps=1
+
+docker run  \
+	--rm \
+  --name=fakecam \
+  --network=fakecam \
+  -p 8080:8080 \
+  -u 1000:39 \
+  $(find /dev -name 'video*' -printf "--device %p ") \
+  fakecam
+
